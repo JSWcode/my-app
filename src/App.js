8688ccc4ae01ef2acd2dbfe997c8d869b5de2367
DESKTOP-QM2CJ38\Jake <jakemac2007@gmail.com>
2024-02-17 21:55:03 +0000

    add image-slider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
-       {/* <Accordian/> */}
+      {/* <Accordian/> */}
 
       {/* Random color component */}
       {/* <RandomColor/> */}
 
-      {/* star rating component */}
-      <StarRating noOfStars={10}/>
+      {/* star rating component
+      <StarRating noOfStars={10} /> */}
+
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={"2"}
+        page={"50"}
+      />
     </div>
   );
 }
